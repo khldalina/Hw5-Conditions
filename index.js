240be554eb58ee3664f026c7dccc299645811ae0
Alina <khldalina@gmail.com>
2022-08-25 15:59:23 +0300

    added new files and js code

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
-const firstNumber = + prompt ('Enter your first number');
-const operation =  prompt ('Enter your operation')
-const secondNumber = + prompt ('Enter your second number');
 let result = 0;
-if (firstNumber) {
-    if (operation === '+')
-        result = firstNumber + secondNumber
-    else if (operation === '-')
-        result = firstNumber - secondNumber
-    else if (operation === '*')
-        result = firstNumber * secondNumber
-    else if (operation === '/')
-        result = firstNumber / secondNumber
-    const sum = `Your result is ${result}`;
-    if (!isNaN(firstNumber)) {
-    }
-    if (!isNaN(secondNumber)) {
-    }
-    alert(sum)
+const firstNumber = + prompt ('Enter your first number');
+const secondNumber = + prompt ('Enter your second number');
+if (isNaN(firstNumber) || isNaN(secondNumber)) {
+    alert('incorrect number')
+}
+const operation =  prompt ('Enter your operation (+ - * /)');
+if (operation!== '-' && operation!== '+' && operation!== '/' && operation!== '*') {
+    alert('incorrect operation')
+} else if (operation === '+') {
+    result = firstNumber + secondNumber
+    alert (`Your result is ${result}.`);
+}else if (operation === '-') {
+    result = firstNumber - secondNumber
+    alert (`Your result is ${result}.`);
+}else if (operation === '*') {
+    result = firstNumber * secondNumber
+    alert (`Your result is ${result}.`);
+}else if (operation === '/') {
+    result = firstNumber / secondNumber
+    alert (`Your result is ${result}.`);
 } else {
-    alert('This operation is not exist')
+    alert('This operation is not exist');
 }
